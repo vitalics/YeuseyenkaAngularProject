8d1aa7978d3dc256f42892c9ebd5284809468fae
DrJekill352 <ckam1997@gmail.com>
2018-01-07 20:36:58 +0300

    Finish make task

--- frontend/src/app/containers/home/home.component.ts
+++ frontend/src/app/containers/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+import { Subject } from 'rxjs/Subject';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+    styleUrls: ['./home.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent {
+    // Заносим HTML элимент с атрибутам id='input' в переменную
+    public inputElement = document.querySelector('#input');
+    // Сорздаём переменную для потока данных
+    public thread$: Subject<any> = new Subject();
 
+    onKey(event: any) {
+
+        let text = event.target.value;
+        if (text.length > 2) {
+            this.thread$.next(text);
+        } else {
+            this.thread$.next('');
+        }
+    }
 }
--- frontend/src/app/containers/shop-list/shop-list.component.ts
+++ frontend/src/app/containers/shop-list/shop-list.component.ts
@@ -1,35 +1,59 @@
 import { BasketSaverService } from '../../services/basketSaver.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 
 import { ShopService, BasketService } from '../../services';
 import { Item } from '@shop/item';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-shop-list',
   templateUrl: 'shop-list.component.html',
-  styleUrls: ['./shop-list.component.css']
+  styleUrls: ['./shop-list.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ShopListComponent implements OnInit {
+  @Input() public thread$: Subject<any>;
+
   public shopList: Item[] = [];
+
   constructor(
     private shopService: ShopService,
     private basketService: BasketService,
     private basketSaveService: BasketSaverService
   ) { }
+
   ngOnInit() {
     this.shopList = this.shopService.getItems();
     // изначально карзина пуста
     console.log(this.basketService.getBasketItems());
+
+    this.thread$.subscribe((searchRequest: string) => {
+      this.searchItem(searchRequest);
+    })
   }
+
+
   // обрабатываем наш предмет из магазина. Выводим в консоль
   public onShopItemClicked(shopItem: Item): void {
     console.log(shopItem);
   }
+
   public onShopItemAddToBasket(item: Item) {
     this.basketService.addItemToBasket(item);
     // смотрим, пуста ли карзина
     console.log(this.basketService.getBasketItems());
     // сохраняем результат при добавлении в корзину
     this.basketSaveService.save(this.basketService.getBasketItems());
+  }
+
+  // Метод который выбирает элементы
+  private searchItem(term: string) {
+    console.log(term);
+    // debugger;
+    if (term == '') {
+      this.shopList = this.shopService.getItems();
+    } else {
+      this.shopList = this.shopList.filter((value: Item) => value.name === term);
+    }
   }
 }
